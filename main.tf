31d3e64c42d3aee2b3e2631cbbaa9b1b1a81ce31
Sakshi_Shilimkar <166102762+sakshishilimkar@users.noreply.github.com>
2026-09-06 11:54:49 +0530

    Refactor AWS resources and add VPC data source
    Removed the ubuntu_server resource and added VPC data source and outputs.

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
-resource "aws_instance" "ubuntu_server" {
-  ami           = var.ami_id
-  instance_type = "t3.micro"
-
+data "aws_vpc" "selected_by_tag"{
+  filter {
+    name = "tag:Name"
+    values = ["terraform"]
+  }
 }
 
+output "vpc_cidr"{
+  values = data.aws_vpc.selected_by_tag.cidr_block
+}
+
+output "vpc_name" {
+  values = data.aws_vpc.selected_by_tag.tags["Name"]
+}
+
+output "vpc_id" {
+  values = data.aws_vpc.selected_by_tag.id
+}
+
+
+resource "aws_instance" "test" {
+  ami = var.ami_id
+  instance_type = var.instance_type
+}
